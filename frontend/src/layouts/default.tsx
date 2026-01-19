7558cf14bda093f0c7b0390b3f5e5f2f88db5a61
EgesaR <egesaraymond644@gmail.com>
2026-01-19 17:33:24 +0300

    added some stuff

--- frontend/src/layouts/default.tsx
+++ frontend/src/layouts/default.tsx
@@ -1,13 +1,13 @@
 import Footer from '@/components/layout/Footer';
-import Navbar from "@/components/layout/Navbar";
+// import Navbar from "@/components/layout/Navbar";
 // import WhatsAppButton from "@/components/WhatsappButton";
 // import React from "react";
 // import { Outlet } from "react-router";
-
+import {gsap} from "gsap"
 const DefaultLayout = () => {
   return (
     <div className='w-full flex flex-col min-h-screen'>
-      <Navbar />
+      {/* <Navbar /> */}
       {/*<div className="flex-1">
     <Outlet />
     <WhatsAppButton />
