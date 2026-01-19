ce4e2c223593ab645f66665a7663f8aa9550c66b
EgesaR <egesaraymond644@gmail.com>
2026-01-19 16:24:02 +0300

    rectified something in the home

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,6 +1,6 @@
 import { getImageUrl } from '@/utils/images';
 import type { Route } from './+types/home';
-import Hero from '@/components/sections/Hero';
+// import Hero from '@/components/sections/Hero';
 // import Services from '@/components/sections/Services';
 // import SmallServices from '@/components/sections/SmallServices';
 // import Tours from '@/components/sections/Tours';
